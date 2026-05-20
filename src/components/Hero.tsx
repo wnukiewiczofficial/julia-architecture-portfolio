b9e1afa7500c3fc323a03432cc5c4d261eb39d0c
Dawid Wnukiewicz <wnukiewiczdev@gmail.com>
2026-05-20 15:15:29 +0200

    Center texts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,7 @@ export default function Hero() {
 	return (
 		<section className={styles.hero}>
 			<p className={styles.eyebrow}>Julia Walczak</p>
-			<h1 className={styles.heading}>
-				Portfolio <br /> Architektoniczne
-			</h1>
+			<h1 className={styles.heading}>Portfolio Architektoniczne</h1>
 			<p className={styles.subtitle}>Architektura krajobrazu</p>
 			<p className={styles.description}>
 				Prace z zakresu projektowania przestrzeni publicznych, ogrodów prywatnych i obiektów małej architektury.
